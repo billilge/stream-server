9546017aaca16982a97c08811f32bea821209533
gain <163835628+leegain1@users.noreply.github.com>
2026-09-12 16:02:01 +0900

    feat: 커서 슬라이스 응답의 nextCursor를 불투명 문자열로 전환

--- core/common/src/main/java/kr/ac/kookmin/stream/common/CursorSliceResult.java
+++ core/common/src/main/java/kr/ac/kookmin/stream/common/CursorSliceResult.java
@@ -2,4 +2,4 @@ package kr.ac.kookmin.stream.common;
 
 import java.util.List;
 
-public record CursorSliceResult<T>(List<T> content, boolean hasNext, Long nextCursor) {}
+public record CursorSliceResult<T>(List<T> content, boolean hasNext, String nextCursor) {}
