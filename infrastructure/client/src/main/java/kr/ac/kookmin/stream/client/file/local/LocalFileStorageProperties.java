41d8a2e860f1363dd99e36956873b98ef4059ebf
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-09-14 12:54:55 +0900

    fix: LocalFileStorageProperties가 prod 프로필에서 등록되지 않도록 제한

--- infrastructure/client/src/main/java/kr/ac/kookmin/stream/client/file/local/LocalFileStorageProperties.java
+++ infrastructure/client/src/main/java/kr/ac/kookmin/stream/client/file/local/LocalFileStorageProperties.java
@@ -1,8 +1,10 @@
 package kr.ac.kookmin.stream.client.file.local;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 
 @ConfigurationProperties(prefix = "file.storage.local")
+@Profile("!prod")
 public record LocalFileStorageProperties(
     String basePath,
     String baseUrl,
