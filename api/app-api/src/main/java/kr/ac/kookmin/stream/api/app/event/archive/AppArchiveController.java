bf891e53e67615aa3c45f7857c682fe4c5da8fb6
sangrae <tiger030205@gmail.com>
2026-09-18 12:36:01 +0900

    feat: 아카이빙 상세 조회 API 추가

--- api/app-api/src/main/java/kr/ac/kookmin/stream/api/app/event/archive/AppArchiveController.java
+++ api/app-api/src/main/java/kr/ac/kookmin/stream/api/app/event/archive/AppArchiveController.java
@@ -4,12 +4,15 @@ import jakarta.validation.Valid;
 import kr.ac.kookmin.stream.ApiErrorCode;
 import kr.ac.kookmin.stream.ApiResponse;
 import kr.ac.kookmin.stream.api.app.event.archive.request.ArchiveListRequest;
+import kr.ac.kookmin.stream.api.app.event.archive.response.ArchiveDetailResponse;
 import kr.ac.kookmin.stream.api.app.event.archive.response.ArchiveListResponse;
 import kr.ac.kookmin.stream.common.CommonErrorCode;
+import kr.ac.kookmin.stream.event.domain.archive.domain.ArchiveErrorCode;
 import kr.ac.kookmin.stream.event.domain.archive.service.ArchiveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,11 @@ public class AppArchiveController {
     @GetMapping
     public ApiResponse<ArchiveListResponse> getArchives(@Valid @ModelAttribute ArchiveListRequest request) {
         return ApiResponse.success(ArchiveListResponse.from(archiveService.getArchives(request.year())));
+    }
+
+    @ApiErrorCode(type = ArchiveErrorCode.class, codes = {"ARCHIVE_NOT_FOUND"})
+    @GetMapping("/{archiveId}")
+    public ApiResponse<ArchiveDetailResponse> getArchive(@PathVariable Long archiveId) {
+        return ApiResponse.success(ArchiveDetailResponse.from(archiveService.getArchive(archiveId)));
     }
 }
